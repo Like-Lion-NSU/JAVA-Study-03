393b4855e0c56148ec89f9861525f7e6b0730315
김성국 <128509842+SUNGGUK@users.noreply.github.com>
2023-04-14 20:37:14 +0900

    Create 3주차 과제 문제 1번.java

---
+++ 성국/3주차 과제 문제 1번.java
@@ -0,0 +1,20 @@
+package likelion;
+
+import java.util.Scanner;
+
+public class problem1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int student = sc.nextInt();
+		
+		for(int i = 1; Sysout.printf(student);i++)
+				System.out.println();
+				/* distinct 조건을 줘서 중복값이 안나오도록 하고
+				1<= 변수 <=30을 선언해서 30까지의 숫자만 나오도록한다음
+				28줄을 프린트하기위해 println을 28줄 출력하려고 했습니다만
+				정답도 아닌 거 같고 구현을 못하겠습니다 죄송합니다*/
+		
+	}
+
+}
