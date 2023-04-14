dc7c8f2a3fa62c9f4afb6e3a4f1c7190980d58eb
junnghyun <118995172+junnghyun@users.noreply.github.com>
2023-04-14 18:04:03 +0900

    Update FlowEx17.java

--- 정현/3주차 주석/FlowEx17.java
+++ 정현/3주차 주석/FlowEx17.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class FlowEx17 {
     public static void main(String[] args) {
-        int num = 0;
+        int num = 0;         // num을 0으로 초기화
 
         System.out.print("*을 출력할 라인의 수를 입력하세요.>");
 
         Scanner scanner = new Scanner(System.in);
-        String tmp = scanner.nextLine();
-        num = Integer.parseInt(tmp);
+        String tmp = scanner.nextLine();   // 입력받은 내용 저장
+        num = Integer.parseInt(tmp);      // 입력받은 문자열을 숫자로 변환
 
-        for(int i=0; i<num; i++) {
-            for(int j=0;j<=i;j++) {
+        for(int i=0; i<num; i++) {        // for문, i가 num보다 작을 때 i가 증가한다
+            for(int j=0;j<=i;j++) {       // for문, j가 i보다 작거나 같을때 j가 증가한다.
                 System.out.print("*");
             }
             System.out.println();
