6c742d2653f2914fb14cd5969c1ac22ebefbf1a9
김성국 <128509842+SUNGGUK@users.noreply.github.com>
2023-04-14 20:39:21 +0900

    Create 문제3번.java

---
+++ 성국/3주차 과제/문제3번.java
@@ -0,0 +1,30 @@
+package likelion;
+
+import java.util.Scanner;
+
+public class Problem3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		 int score = sc.nextInt();
+//		int a = score;
+//		int b = score;
+//		int c = score;
+//		int d = score;
+//		int e = score;
+//		int f = score;
+//		int g = score;
+//		int h = score;
+//		int i = score;
+//		int j = score;
+//		if(score%42 == score) {
+//				System.out.println();
+//		}
+				System.out.println(score%42);
+				//count(score equals 서로)
+				//score <= 1000 && score != -
+				//score는 1000보다 작거나 같고 음이 아닌 정수 조건
+				//for문은 숫자가 하나씩만 증감할 수 있으니까 변수 10가지를 
+				//어떻게 설정해야할지 모르겠습니다
+	
+	}
