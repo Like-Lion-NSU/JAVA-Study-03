708596efbfde395d0d18d458de847fd80f95f417
junnghyun <118995172+junnghyun@users.noreply.github.com>
2023-04-14 18:07:37 +0900

    2번

--- 정현/3주차 주석/FlowEx28.java
+++ 정현/3주차 주석/FlowEx28.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class FlowEx28 {
     public static void main(String[] args) {
-        int input =0, answer = 0;
+        int input =0, answer = 0;            // input와 answer을 0으로 초기화
         
         answer = (int) (Math.random() * 100) + 1;     //1~100사이의 임의의 수를 저장
         Scanner scanner = new Scanner(System.in);
@@ -12,11 +12,11 @@ public class FlowEx28 {
             input = scanner.nextInt();
             
             if(input > answer) {
-                System.out.println("더 작은 수로 다시 시도해보세요.");
+                System.out.println("더 작은 수로 다시 시도해보세요.");  // input가 answer보다 클때 실행
             } else if (input < answer) {
-                System.out.println("더 큰 수로 다시 시도해보세요.");
+                System.out.println("더 큰 수로 다시 시도해보세요.");    // input가 answer보다 작을 때 실행
             }
-        } while(input !=answer);
+        } while(input !=answer);        // input과 answer의 값이 같을때까지 반복하고 같을 경우 밑에있는 코드 실행
         
         System.out.println("정답입니다.");
     }
