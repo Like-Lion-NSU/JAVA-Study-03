18bcab2a7c916bf4f95823d8361a9ac373215ebc
junnghyun <118995172+junnghyun@users.noreply.github.com>
2023-04-14 18:14:33 +0900

    6번

--- 정현/3주차 주석/test5_23.java
+++ 정현/3주차 주석/test5_23.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 public class test5_23 {
     public static void main(String[] args) {
         String[][] words = {
-                {"chair","의자"},
-                {"computer","컴퓨터"},
-                {"integer","정수"}
+                {"chair","의자"},          //words[0] [0], words[0][1]
+                {"computer","컴퓨터"},     //words[1] [0], words[1][1]
+                {"integer","정수"}         //words[2] [0], words[2][1]
         };
 
         Scanner scanner = new Scanner(System.in);
 
-        for(int i=0; i<words.length; i++) {
-            System.out.printf("Q5d. %s의 뜻은?", i+i, words[i][0]);
+        for(int i=0; i<words.length; i++) {              // i가 배열의 길이보다 작을 때 i 증가
+            System.out.printf("Q%d. %s의 뜻은?", i+i, words[i][0]);
 
             String tmp = scanner.nextLine();
 
-            if(tmp.equals(words[i][1])) {
+            if(tmp.equals(words[i][1])) {          // words 답과 같으면 밑에 코드 실행
                 System.out.printf("정답입니다.%n%n");
             }else {
-                System.out.printf("틀렸습니다. 정답은 %s입니다.%n%n", words[i][1]);
+                System.out.printf("틀렸습니다. 정답은 %s입니다.%n%n", words[i][1]);  // 답이 틀리면 코드 실행
             }
         }
     }
