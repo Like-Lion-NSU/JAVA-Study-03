0eed4a8b25abf5ed0c4c6f49ca57a9fe52b437eb
junnghyun <118995172+junnghyun@users.noreply.github.com>
2023-04-14 17:46:54 +0900

    5번

---
+++ 정현/3주차 과제/ch03test05.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ch03test05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+
+        if (i >= 90 && i <= 100) {
+            System.out.println("A");
+        } else if (i >= 80 && i <= 89) {
+            System.out.println("B");
+        } else if (i >= 70 && i <= 79) {
+            System.out.println("C");
+        } else if (i >= 60 && i <= 69) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
