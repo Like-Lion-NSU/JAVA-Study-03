a339a285ed603b0c6c3a47c0fdb0a67db61ac385
junnghyun <118995172+junnghyun@users.noreply.github.com>
2023-04-14 17:41:32 +0900

    4번

---
+++ 정현/ch03test04.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ch03test04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("A: ");
+        int A = sc.nextInt();
+        System.out.print("B: ");
+        int B = sc.nextInt();
+
+        if (A > B) {
+            System.out.println(">");
+        } else if (A < B) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
